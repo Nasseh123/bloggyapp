181dcb859c548dbb0f45dad37533eed477453cb7
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-05-11 15:46:50 +0300

    Created a profile page and anupdate page where one can update there details and also see about there account while in the app

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from .import main
 from flask_login import login_required
+from .forms import UpdateProfile
+from ..import db
+from ..models import User
 @main.route('/')
 @login_required
 def index():
@@ -10,3 +13,31 @@ def index():
     title="Blog App"
     message="Welcome to blog app"
     return render_template('index.html',title=title,message=message)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
