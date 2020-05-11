58fd61bcd8992694c05673b3fae19549cda9613f
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-05-11 15:02:39 +0300

    INstalled flask login extension that is used for user authentication and initialized the extension to the create app function to enable the use of it across the apps files and folders and various function.Then created a authenticated route fr one to access the details of the app the user must have logged in to be accepted to see the information in the application.Imported a class that enable sthe working of the login extension and making the authentication process.Made the forms and function necessary for login of users and registering of users.Created the temPLATE FILES FOor having the signup form and login form.Created the routes for the registration and signin

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 from flask import render_template,request,redirect,url_for
 from .import main
-
+from flask_login import login_required
 @main.route('/')
+@login_required
 def index():
     """
     """
--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 export SECRET_KEY='blog'
 
 python3.6 manage.py server
-"pour me out"
-Naomi Raine
